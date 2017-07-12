ca56072f44f74be9c4065f6416c13ba61f44fcca
Aaron Billings <superswiftdeveloper@gmail.com>
2017-07-12 13:03:29 -0400

    Added Firebase Support
    Added Firebase backend to App, installed pod files for firebase.

---
+++ Pods/Target Support Files/Pods-SampleSocial/Pods-SampleSocial-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleSocial : NSObject
+@end
+@implementation PodsDummy_Pods_SampleSocial
+@end
---
+++ SampleSocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  SampleSocial
+//
+//  Created by Aaron Billings on 7/12/17.
+//  Copyright © 2017 Aaron Billings. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
