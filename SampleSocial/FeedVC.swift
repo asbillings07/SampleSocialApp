4048be1473e8e4014318b070eecfb7dbd187c9a9
Aaron Billings <superswiftdeveloper@gmail.com>
2017-07-21 10:06:24 -0400

    Firebase Display
    Completed Firebase Display

--- SampleSocial/FeedVC.swift
+++ SampleSocial/FeedVC.swift
@@ -62,8 +62,18 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UITe
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row]
-        print("AARON: \(post.caption)")
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            
+            cell.configureCell(post: post)
+            
+            return cell
+            
+        } else {
+            
+            return PostCell()
+        }
+      
     }
     
     
